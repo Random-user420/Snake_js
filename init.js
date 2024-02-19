a48e966b63dac9a387388f77b5aa699431a38744
Random-user420 <leon_schmid@tutanota.com>
2024-02-19 14:28:57 +0100

    fixed scaling

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 const main_gr = document.getElementById("main-grid");
 const zeilen = 33;
-const spalten = Math.floor(((window.innerWidth / window.innerHeight) * 33.333333) * 0.9);
+const spalten = Math.floor(((window.innerWidth / window.innerHeight) * 33.3333333) * 0.90);
 let snake_arr = [[3, 1], [1, 1], [2, 1]];
 let direction = [1, 0];
 let direction_chanched = false;
