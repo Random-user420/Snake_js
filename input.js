a26e3fdd204b3d0e1711d248de144213c35f7cc0
Random-user420 <leon_schmid@tutanota.com>
2024-02-20 21:07:09 +0100

    added comments

--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 
 function d_right() {
-    if (direction[0] === 0) {
+    if (direction[0] === 0) { //to prevent that the snake goes in the opposite diection of the current
         direction = [1, 0];
     }
 }
@@ -21,17 +21,17 @@ function d_down() {
 }
 
 function swipe(e) {
-    const touch_e_x = e.changedTouches[0].clientX;
+    const touch_e_x = e.changedTouches[0].clientX; //the coordinate at the end of the touch event
     const touch_e_y = e.changedTouches[0].clientY;
 
-    if (Math.abs(touch_e_x - touch_s_x) >= Math.abs(touch_e_y - touch_s_y)) {
-        if (touch_e_x - touch_s_x >= touch_threshold && !direction_chanched) { direction = [1, 0]; }
-        else if (touch_s_x - touch_e_x >= touch_threshold && !direction_chanched) { direction = [-1, 0]; }
-        else if (Math.abs(touch_e_x - touch_s_x) <= touch_pause_threshold) { pause(); }
+    if (Math.abs(touch_e_x - touch_s_x) >= Math.abs(touch_e_y - touch_s_y)) { //wich diection is grater
+        if (touch_e_x - touch_s_x >= touch_threshold && !direction_chanched) { direction = [1, 0]; } //if to right is more than the touch_threshold
+        else if (touch_s_x - touch_e_x >= touch_threshold && !direction_chanched) { direction = [-1, 0]; } //if to left is more than the touch_threshold
+        else if (Math.abs(touch_e_x - touch_s_x) <= touch_pause_threshold) { pause(); } //if its under te pause threshold then pause
     }
-    else if (touch_e_y - touch_s_y >= touch_threshold && !direction_chanched) { direction = [0, 1]; }
-    else if (touch_s_y - touch_e_y >= touch_threshold && !direction_chanched) { direction = [0, -1]; }
-    else if (Math.abs(touch_s_y - touch_e_y) <= touch_pause_threshold) { pause(); }
+    else if (touch_e_y - touch_s_y >= touch_threshold && !direction_chanched) { direction = [0, 1]; } //if to down is more than the touch_threshold
+    else if (touch_s_y - touch_e_y >= touch_threshold && !direction_chanched) { direction = [0, -1]; } //if to up is more than the touch_threshold
+    else if (Math.abs(touch_s_y - touch_e_y) <= touch_pause_threshold) { pause(); } //if its under te pause threshold then pause
 }
 
 window.addEventListener("keydown", function (event) {
@@ -43,7 +43,7 @@ window.addEventListener("keydown", function (event) {
             pause();
             break;
         case 'w':
-            if (!direction_chanched) { d_up(); }
+            if (!direction_chanched) { d_up(); } //to prevent that the diection gets changed twice bevore the snake moved
             direction_chanched = true;
             break;
         case 'a':
@@ -65,7 +65,7 @@ let touch_s_x
 let touch_s_y
 
 document.addEventListener("touchstart", function (event) {
-    event.preventDefault();
+    event.preventDefault(); //should prevent zooming and reloading of the page
     touch_s_x = event.touches[0].clientX;
     touch_s_y = event.touches[0].clientY;
 })
